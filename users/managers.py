ed29b06048895504d21450db1dbe19a8308b79fe
Matteo Pompilio <matteo.pompilio@mail.polimi.it>
2024-11-13 22:43:10 +0100

    Finalized User creation when profile.is_esner is true

--- users/managers.py
+++ users/managers.py
@@ -4,21 +4,20 @@ from django.utils.translation import gettext_lazy as _
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, profile, password, **extra_fields):
         """
         Create and save a user with the given email and password.
         """
 
-        if not email:
-            raise ValueError(_("The email must be set"))
+        if not profile:
+            raise ValueError(_("The profile must be set"))
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(profile=profile, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, profile, password, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -32,4 +31,4 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(profile, password, **extra_fields)
