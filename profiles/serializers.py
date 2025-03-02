0c374f600a4a7c97a76eafb1ffe8461cbe1ad71f
Matteo Pompilio <matteo.pompilio@mail.polimi.it>
2025-03-02 16:21:58 +0100

    CRUD Documents handling; Document model in admin panel; permissions documentation

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from django_countries.serializer_fields import CountryField
+from rest_framework.fields import SerializerMethodField
+
 from profiles.models import Profile, Document, Matricola
 from treasury.serializers import ESNCardSerializer
 
-# Serializers are classes that take an object from the database and 
+# Serializers are classes that take an object from the database and
 # turn it into json (or other formats) in order to be sent through the API.
 # Serializers can also do the opposite: they turn API data into database objects.
 # There are different serializers for different use cases. For example some fields cannot 
@@ -24,7 +26,7 @@ class MatricolaViewSerializer(serializers.ModelSerializer):
         model = Matricola
         exclude = ['profile']
 
-# Serializer to create documents 
+# Serializer to create documents
 # Takes the data from API and creates an object
 class DocumentCreateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -39,30 +41,45 @@ class MatricolaCreateSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['id','created_at','updated_at','enabled']
 
-# Serializer to edit documents 
+# Serializer to edit documents
 class DocumentEditSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
-        fields = ['number','expiration']
+        fields = '__all__'
 
 # Serializer to edit matricole
 class MatricolaEditSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricola
         exclude = ['number','exchange_end']
-        
+
 
 # Serializer to view a profile in detail (i.e. including all esncards, documents and matricole),  
 class ProfileDetailViewSerializer(serializers.ModelSerializer):
+    esncards = SerializerMethodField()
+    documents = SerializerMethodField()
+    matricole = SerializerMethodField()
+    country = CountryField()
+
     class Meta:
         model = Profile
         fields = '__all__'
-        read_only_views = ['id','created_at','updated_at','enabled','esncards','documents','matricole']
+        read_only_views = ['id', 'created_at', 'updated_at', 'enabled', 'esncards', 'documents', 'matricole']
 
-    esncards = ESNCardSerializer(source='esncard_set',many=True)
-    documents = DocumentViewSerializer(source='document_set',many=True)
-    matricole = MatricolaViewSerializer(source='matricola_set',many=True)
-    country = CountryField()
+    @staticmethod
+    def get_esncards(obj):
+        enabled_esncards = obj.esncard_set.filter(enabled=True)
+        return ESNCardSerializer(enabled_esncards, many=True).data
+
+    @staticmethod
+    def get_documents(obj):
+        enabled_documents = obj.document_set.filter(enabled=True)
+        return DocumentViewSerializer(enabled_documents, many=True).data
+
+    @staticmethod
+    def get_matricole(obj):
+        enabled_matricole = obj.matricola_set.filter(enabled=True)
+        return MatricolaViewSerializer(enabled_matricole, many=True).data
 
 # Serializer to view a profile overview (i.e. including just the latest esncard, document, matricola)
 class ProfileListViewSerializer(serializers.ModelSerializer):
