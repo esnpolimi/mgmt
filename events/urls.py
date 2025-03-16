23a3c2875d82bf7dfb04603aa28361f7fa42ae1b
Matteo Pompilio <matteo.pompilio@mail.polimi.it>
2025-03-16 10:54:15 +0100

    First Event detail page

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from events import views
 urlpatterns = [
     path('events/', views.events_list),
     path('event/',  views.event_creation),
-    path('event/<str:pk>', views.event_detail), 
+    path('event/<str:pk>/', views.event_detail),
     path('form_subscription/', views.form_subscription_creation),
     path('subscription/',views.manual_subscription_creation),
     path('subcription/<str:pk>', views.subscription_detail),
