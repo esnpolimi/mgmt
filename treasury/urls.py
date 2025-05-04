9998b7e620db878247a616d49f0b52f4fc665277
Matteo Pompilio <matteo.pompilio@mail.polimi.it>
2025-05-04 22:35:51 +0200

    Transactions dashboard component; improved Accounts list&component

--- treasury/urls.py
+++ treasury/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     path('esncard_emission/',views.esncard_emission),
     path('esncard/<str:pk>/', views.esncard_detail),
     path('transaction/', views.transaction_add),
-    path('transaction/<str:pk>/', views.transaction_detail ),
+    path('transaction/<str:pk>/', views.transaction_detail),
     path('transactions/', views.transactions_list),
     path('accounts/', views.accounts_list),
+    path('accounts_full/', views.accounts_list_full),
     path('account/', views.account_creation),
     path('account/<str:pk>/', views.account_detail),
 ]
