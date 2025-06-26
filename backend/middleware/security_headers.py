6c45333c9704ed489be0b6e2bf142ca10500798c
Matteo Pompilio <matteo.pompilio@mail.polimi.it>
2025-06-26 21:40:37 +0200

    Security measures for prod v2

--- backend/middleware/security_headers.py
+++
@@ -1,8 +0,0 @@
-class SecurityHeadersMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        response["Permissions-Policy"] = "camera=(), microphone=(), geolocation=()"
-        return response
