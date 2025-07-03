5b9458774bdc79453e99ae9580aba1b5b7e88450
Matteo Pompilio <matteo.pompilio@mail.polimi.it>
2025-07-03 14:47:33 +0200

    Homepage refactored + manifest updated, with ESN logo icons

--- backend/urls.py
+++ backend/urls.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("backend/", include('users.urls')),
     path("backend/", include('profiles.urls')),
     path("backend/", include('treasury.urls')),
-    path("backend/", include('events.urls')),
-    path("sentrydebug/", trigger_error),
+    path("backend/", include('events.urls'))
 ]
