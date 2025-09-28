afa7760b3f249afc0684757a067aeccba52a4a95
Matteo Pompilio <matteo.pompilio@mail.polimi.it>
2025-09-28 10:57:30 +0200

    Vite env files removed

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,18 @@
 import * as Sentry from "@sentry/react";
 
 if (import.meta.env.MODE === 'production') {
+    // Set VITE_SENTRY_DSN and VITE_API_HOST manually
+    window.API_HOST = "https://mgmt.esnpolimi.it/backend";
     Sentry.init({
-        dsn: import.meta.env.VITE_SENTRY_DSN,
+        dsn: "https://f7ee9b97bae2c35e767d8e156eb7b116@o4509581352828928.ingest.de.sentry.io/4509581461487696",
         sendDefaultPii: true,
         environment: 'production'
     });
+}
+
+else {
+    // Set VITE_API_HOST manually
+    window.API_HOST = "http://localhost:8000/backend";
 }
 
 import ReactDOM from 'react-dom/client';
