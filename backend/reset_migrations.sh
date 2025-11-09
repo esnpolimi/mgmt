0b9dbf9ef5c5544637f1bfb323d13480752fe60c
Moussa Gerges <moussagerges9@gmail.com>
2025-11-09 16:18:18 +0100

    clear buil frontend

--- backend/reset_migrations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-
-rm db.sqlite3
-
-python manage.py makemigrations
-python manage.py migrate
