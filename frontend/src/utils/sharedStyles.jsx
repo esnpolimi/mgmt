972cce4235302ab12f990eb49b0c46d30407bf7e
Moussa Gerges <moussagerges9@gmail.com>
2025-11-09 14:28:53 +0100

    fixing problems with moving subscriptions in events

--- frontend/src/utils/sharedStyles.jsx
+++ frontend/src/utils/sharedStyles.jsx
@@ -20,12 +20,15 @@ export const styleESNcardModal = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
+    maxWidth: '90vw',
+    maxHeight: '90vh',
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
     outline: 'none',
     display: 'flex',
     flexDirection: 'column',
+    overflow: 'auto',
 };
 
 // Shared color options
